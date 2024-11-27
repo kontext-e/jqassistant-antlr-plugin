942789cc94368640cde7f01d2e62f44297927905
WilliamThimm <William.Thimm@gmail.com>
2024-11-27 15:56:45 +0100

    Generating lexer and parser through java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,35 @@
+import groovy.lang.GroovyClassLoader;
+import org.antlr.v4.Tool;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    GroovyClassLoader loader = new GroovyClassLoader();
 
     public static void main(String[] args) {
 
+        String grammarName = "Logging";
+        String lexerParserDirectory = generateParser(grammarName);
     }
+
+
+    private static String generateParser(String grammarName) {
+        String grammarFileName = String.format("src/main/antlr4/%s.g4", grammarName);
+        String outputDirectory = "src/main/antlr4/" + grammarName;
+
+        List<String> arguments = new ArrayList<>();
+        arguments.add(new File(grammarFileName).getAbsolutePath());
+        arguments.add("-o");
+        arguments.add(outputDirectory);
+
+        Tool tool = new Tool(arguments.toArray(new String[0]));
+        tool.processGrammarsOnCommandLine();
+
+        return outputDirectory;
+    }
+
+
 }
