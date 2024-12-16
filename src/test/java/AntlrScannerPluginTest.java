db8d08c178a13d9b26e2201137436a182ddc68e8
WilliamThimm <William.Thimm@gmail.com>
2024-12-16 16:11:27 +0100

    Added second testing grammar

--- src/test/java/AntlrScannerPluginTest.java
+++ src/test/java/AntlrScannerPluginTest.java
@@ -11,14 +11,19 @@ class AntlrScannerPluginTest extends AbstractPluginIT {
     @Test
     void scan() {
         File file = new File("src/test/resources/logging/Logging.g4");
+        File file2 = new File("src/test/resources/dot/DOT.g4");
 
         Map<String, Object> properties = Map.of(
                 "jqassistant.plugin.antlr.createNodesContainingEmptyText", true,
                 "\"jqassistant.plugin.antlr.grammars\"[0].grammar", "Logging.g4",
                 "\"jqassistant.plugin.antlr.grammars\"[0].grammarRoot", "log",
-                "\"jqassistant.plugin.antlr.grammars\"[0].fileEnding", ".logging"
+                "\"jqassistant.plugin.antlr.grammars\"[0].fileEnding", ".logging",
+                "\"jqassistant.plugin.antlr.grammars\"[1].grammar", "DOT.g4",
+                "\"jqassistant.plugin.antlr.grammars\"[1].grammarRoot", "graph",
+                "\"jqassistant.plugin.antlr.grammars\"[1].fileEnding", ".dot"
         );
-        getScanner(properties).scan(file, "/Logging.g4", null);
+//        getScanner(properties).scan(file, "/Logging.g4", null);
+        getScanner(properties).scan(file2, "/calculator.g4", null);
 
         store.beginTransaction();
         Query.Result<Query.Result.CompositeRowObject> result = store.executeQuery("MATCH (n:Antlr) RETURN n");
