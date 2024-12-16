4a2cf8ebe41ca5f9c57320c792a624cd935b250e
WilliamThimm <William.Thimm@gmail.com>
2024-12-16 16:33:21 +0100

    Minor Code Cleanup

--- src/main/java/de/kontext_e/jqassistant/plugin/antlr/api/model/AntlrDescriptor.java
+++ src/main/java/de/kontext_e/jqassistant/plugin/antlr/api/model/AntlrDescriptor.java
@@ -7,6 +7,7 @@ import com.buschmais.xo.neo4j.api.annotation.Relation;
 import java.util.List;
 
 @Label("Antlr")
+@SuppressWarnings("unused")
 public interface AntlrDescriptor extends Descriptor {
 
     String getText();
