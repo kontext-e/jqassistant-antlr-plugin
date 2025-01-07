3d5b213329621271d7540b137a9919281f258cc2
WilliamThimm <William.Thimm@gmail.com>
2025-01-07 17:35:29 +0100

    Added first proper Test

--- src/test/java/AntlrScannerPluginTest.java
+++ src/test/java/AntlrScannerPluginTest.java
@@ -1,47 +1,56 @@
+import com.buschmais.jqassistant.core.scanner.api.DefaultScope;
 import com.buschmais.jqassistant.core.test.plugin.AbstractPluginIT;
-import com.buschmais.xo.api.Query;
+import com.buschmais.jqassistant.plugin.common.api.model.FileDescriptor;
+import de.kontext_e.jqassistant.plugin.antlr.api.model.AntlrDescriptor;
+import de.kontext_e.jqassistant.plugin.antlr.api.model.GrammarFileDescriptor;
+import de.kontext_e.jqassistant.plugin.antlr.api.model.ScannedFileDescriptor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 class AntlrScannerPluginTest extends AbstractPluginIT {
 
-    @Test
-    void scan() {
-        File file = new File("src/test/resources/logging/Logging.g4");
-        File file2 = new File("src/test/resources/dot/DOT.g4");
-        File file3 = new File("src/test/resources/calculator/calculator.g4");
+    File file = new File("src/test/resources/logging/Logging.g4");
+    Map<String, Object> properties = Map.of(
+            "\"jqassistant.plugin.antlr.grammars\"[0].grammar", "Logging.g4",
+            "\"jqassistant.plugin.antlr.grammars\"[0].grammarRoot", "log",
+            "\"jqassistant.plugin.antlr.grammars\"[0].fileExtension", ".logging"
+    );
 
-        Map<String, Object> properties = Map.of(
-                "jqassistant.plugin.antlr.createNodesContainingEmptyText", true,
-                "\"jqassistant.plugin.antlr.grammars\"[0].grammar", "Logging.g4",
-                "\"jqassistant.plugin.antlr.grammars\"[0].grammarRoot", "log",
-                "\"jqassistant.plugin.antlr.grammars\"[0].fileExtension", ".logging",
-                "\"jqassistant.plugin.antlr.grammars\"[1].grammar", "DOT.g4",
-                "\"jqassistant.plugin.antlr.grammars\"[1].grammarRoot", "graph",
-                "\"jqassistant.plugin.antlr.grammars\"[1].fileExtension", ".dot",
-                "\"jqassistant.plugin.antlr.grammars\"[2].grammar", "calculator.g4",
-                "\"jqassistant.plugin.antlr.grammars\"[2].grammarRoot", "equation",
-                "\"jqassistant.plugin.antlr.grammars\"[2].fileExtension", ".txt"
-        );
-//        getScanner(properties).scan(file, "/Logging.g4", null);
-//        getScanner(properties).scan(file2, "/DOT.g4", null);
-        getScanner(properties).scan(file3, "/calculator.g4", null);
-
+    @BeforeEach
+    void setUp() {
         store.beginTransaction();
-        Query.Result<Query.Result.CompositeRowObject> result = store.executeQuery("MATCH (n:Antlr) RETURN n");
-        result.iterator().forEachRemaining(rowObject -> System.out.println(rowObject.toString()));
+        FileDescriptor fileDescriptor = store.create(FileDescriptor.class);
+        getScanner(properties).scan(file, fileDescriptor, file.getAbsolutePath(), DefaultScope.NONE);
+    }
 
-        Query.Result<Query.Result.CompositeRowObject> result1 = store.executeQuery("MATCH (n:Antlr)-[:HAS_CHILD]->(m:Antlr) RETURN n, m");
-        result1.iterator().forEachRemaining(rowObject -> System.out.println(rowObject.toString()));
-
-        Query.Result<Query.Result.CompositeRowObject> compositeRowObjects = store.executeQuery("MATCH (n:Antlr) RETURN count(n)");
-        compositeRowObjects.iterator().forEachRemaining(rowObject -> System.out.println(rowObject.toString()));
-
-        store.executeQuery("Match (n) Detach Delete n");
-
+    @AfterEach
+    void tearDown() {
         store.commitTransaction();
+        store.beginTransaction();
+        query("Match (n) Detach Delete n");
+        store.commitTransaction();
+    }
+
+    @Test
+    void testRelationsBetweenFilesAndTree() {
+        String query = "MATCH (n:Grammar:Antlr:File) RETURN n";
+        TestResult result = query(query);
+        List<GrammarFileDescriptor> grammarFiles = result.getColumn("n");
+        assertThat(grammarFiles).hasSize(1);
+
+        var grammarFileDescriptor = grammarFiles.get(0);
+        var scannedFiles = grammarFileDescriptor.getScannedFiles();
+        assertThat(scannedFiles).hasSize(1);
+
+        var scannedFileDescriptor = scannedFiles.get(0);
+        assertThat(scannedFileDescriptor.getChildren().size()).isGreaterThan(0);
     }
 }
